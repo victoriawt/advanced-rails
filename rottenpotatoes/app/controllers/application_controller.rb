80c58e6dd7d6d1d04c9dce2bef95145a9b9e8b1f
Victoria Tsou <vt6666@gmail.com>
2014-11-09 16:32:23 -0600

    Fixed login.feature errors, working on Twitter

--- rottenpotatoes/app/controllers/application_controller.rb
+++ rottenpotatoes/app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_filter :set_current_user
+ protect_from_forgery  
+ before_filter :set_current_user
   protected # prevents method from being invoked by a route
   def set_current_user
     # we exploit the fact that find_by_id(nil) returns nil
     @current_user ||= Moviegoer.find_by_id(session[:user_id])
-    redirect_to login_path and return unless @current_user
+   #redirect_to login_path and return unless @current_user
+  end
+ private
+  if Rails.env.test?
+    prepend_before_filter :stub_current_user
+    def stub_current_user
+      session[:user_id] ||= cookies[:stub_user_id] 
+    end
   end
 end
 
--- rottenpotatoes/features/step_definitions/login_steps.rb
+++ rottenpotatoes/features/step_definitions/login_steps.rb
@@ -1,3 +1,15 @@
 Given /^I am not logged in$/ do
 
 end
+
+Given(/^I am logged in as "([^"]*)"$/) do |name|
+  @current_user = Moviegoer.create!(:name => name)
+  log_in
+end
+
+private
+def log_in
+  cookie_jar = Capybara.current_session.driver.browser.current_session.
+                 instance_variable_get(:@rack_mock_session).cookie_jar
+  cookie_jar[:stub_user_id] = @current_user.id
+end
