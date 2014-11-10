628b7e3c97e2c312610106bb63564072afbabc08
Victoria Tsou <vt6666@gmail.com>
2014-11-10 14:19:52 -0600

    Showing movie reviews implemented

--- rottenpotatoes/app/models/moviegoer.rb
+++ rottenpotatoes/app/models/moviegoer.rb
@@ -6,4 +6,9 @@ class Moviegoer < ActiveRecord::Base
       :uid => auth["uid"],
       :name => auth["info"]["name"])
   end
+# place a copy of the following line anywhere inside the Movie class
+#  AND inside the Moviegoer class (idiomatically, it should go right
+#  after 'class Movie' or 'class Moviegoer'):
+  has_many :reviews
+
 end
